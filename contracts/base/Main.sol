a6fbee32be34343118f9e045d80c23b5dae1069b
Anubhav <anubhavgirdhar@gmail.com>
2020-02-09 23:46:17 +0530

    added erc20 and custom logic

--- contracts/base/Main.sol
+++ contracts/base/Main.sol
@@ -1,69 +1,135 @@
-pragma solidity >=0.5.0 <0.6.0;
+/*
+Implements EIP20 token standard: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+.*/
 
+pragma solidity ^0.4.21;
 
-interface MintInterface {
-       function mint(uint) external returns (uint);
-       function redeemUnderlying(uint redeemAmount) external returns (uint);
-       function balanceOf(address owner) external view returns (uint);
-}  
+import "./EIP20Interface.sol";
+import "./ctokenInterface.sol";
 
-interface TokenInterface {
-    function allowance(address, address) external view returns (uint);
-    function approve(address, uint) external;
-    function transfer(address, uint) external returns (bool);
-    function transferFrom(address, address, uint) external returns (bool);
+contract pouch is EIP20Interface {
+    uint256 private constant MAX_UINT256 = 2**256 - 1;
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public allowed;
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name; //fancy name: eg Simon Bucks
+    uint8 public decimals; //How many decimals to show.
+    string public symbol; //An identifier: eg SBX
+
+    address daiAddress = 0xB5E5D0F8C0cbA267CD3D7035d6AdC8eBA7Df7Cdd;
+    address cDaiAddress = 0x2B536482a01E620eE111747F8334B395a42A555E;
+
+    EIP20Interface daiToken = EIP20Interface(daiAddress);
+    ctokenInterface cDai = ctokenInterface(cDaiAddress);
+
+    uint256 cDaiAllowedAmount = 35000000000000000000000000000000000000000000000000;
+
+    constructor(string _tokenName, uint8 _decimalUnits, string _tokenSymbol)
+        public
+    {
+        name = _tokenName; // Set the name for display purposes
+        decimals = _decimalUnits; // Amount of decimals for display purposes
+        symbol = _tokenSymbol; // Set the symbol for display purposes
+
+        daiToken.approve(cDaiAddress, cDaiAllowedAmount);
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        uint256 allowance = allowed[_from][msg.sender];
+        require(balances[_from] >= _value && allowance >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        if (allowance < MAX_UINT256) {
+            allowed[_from][msg.sender] -= _value;
+        }
+        emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256 remaining)
+    {
+        return allowed[_owner][_spender];
+    }
+
+    function deposit(uint256 value) external {
+        // Check if User's Dai Balance is more or equal to the value sent.
+        uint256 userBalance = daiToken.balanceOf(msg.sender);
+        require(
+            userBalance >= value,
+            "User does not have the required DAI balance."
+        );
+
+        daiToken.transferFrom(msg.sender, address(this), value);
+        balances[msg.sender] += value;
+        totalSupply += value;
+        emit Transfer(
+            0x0000000000000000000000000000000000000000,
+            msg.sender,
+            value
+        );
+
+        // Check for Dai allowance given from this contract to Cdai Contract
+        uint256 cDaiAllowance = daiToken.allowance(address(this), cDaiAddress);
+        if (cDaiAllowance < value) {
+            uint256 amount = value > cDaiAllowedAmount
+                ? value
+                : cDaiAllowedAmount;
+            daiToken.approve(cDaiAddress, amount);
+        }
+
+        cDai.mint(value);
+    }
+
+    function withdraw(uint256 value) external {
+        // Check if User's PCH balance is more or equal to the value sent.
+        uint256 pouchBalance = balanceOf(msg.sender);
+        require(
+            pouchBalance >= value,
+            "User does not have the required PCH balance."
+        );
+
+        // Burn PCH
+        transfer(0x0000000000000000000000000000000000000000, value);
+        totalSupply -= value;
+
+        // Redeem User's DAI from compound and transfer it to user.
+        cDai.redeemUnderlying(value);
+        daiToken.transfer(msg.sender, value);
+    }
 
 }
-
-contract PayZap {
-    uint public totalDaiDeposits; 
-    mapping (address => bool) registeredUser;
-    mapping(address=>uint) balances;
-    address daiAddress = 0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa;
-    address cDaiAddress = 0x6D7F0754FFeb405d23C51CE938289d4835bE3b14;
-
-      MintInterface cDai = MintInterface(cDaiAddress);
-      TokenInterface dai = TokenInterface(daiAddress);
-         
-    function checkDaiAllowance() view external returns(uint){
-        return dai.allowance(msg.sender, address(this));
-    }
-    
-    
-    function userBalance() view external returns (uint) {
-        return balances[msg.sender];
-    }
-
-    function deposit(uint amount) external {
-        dai.transferFrom(msg.sender, address(this), amount);
-        totalDaiDeposits += amount;
-        dai.approve(cDaiAddress,amount);
-        cDai.mint(amount);
-        balances[msg.sender] += amount;
-        registeredUser[msg.sender] = true;
-    }
-    
-    
-
-    
-    function transact(address recipient, uint amount)external {
-        require(registeredUser[msg.sender] == true, 'Sender not registered');
-        require(amount <= balances[msg.sender], 'Insufficient Funds');
-        balances[msg.sender] -= amount;
-        balances[recipient] += amount;
-    }
-    
-    function withdraw(uint amount) external {
-        require(amount <= balances[msg.sender], 'Insufficient Funds');
-         cDai.redeemUnderlying(amount);
-         dai.transfer(msg.sender, amount);
-         balances[msg.sender] -= amount;
-    }
-    
-    function contractBalance() view external returns (uint) {
-        return cDai.balanceOf(address(this));
-    }
-    
-}
-    
-    
